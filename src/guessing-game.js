42c7c7d607d8b79a841134c3956b04a4dd26f609
Olga Kapichenka <oshrub@gmail.com>
2024-09-30 14:58:07 +0200

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        return this.currentGuess = Math.round((this.minValue + this.maxValue) / 2);
     }
 
     lower() {
-
+        return this.maxValue = this.currentGuess;
     }
 
     greater() {
-
+        return this.minValue = this.currentGuess;
     }
 }
-
 module.exports = GuessingGame;
